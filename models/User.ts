a3b05ab048194eab93e26dbb7033d74507f59ff8
Naman Matoliya <naman@fieldproxy.com>
2026-03-27 14:03:06 +0530

    first commit

---
+++ models/User.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import { Schema, model, models } from "mongoose";
+
+export interface IUser{
+    email: string;
+    password: string;
+    _id?: mongoose.Types.ObjectId;
+    createdAt?:Date;
+    updatedAt?:Date;
+}
+
+const userSchema = new Schema<IUser>(
+{
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+}, {
+    timestamps: true
+})
+
+  
