5fa478017110f5ae84f3cdf104507301eafd4e31
Naman Matoliya <naman@fieldproxy.com>
2026-03-31 18:28:45 +0530

    db connect

---
+++ lib/db.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!
+
+if(!MONGODB_URI){
+    throw new Error("Please define mongo URI in env variables")
+}
+let cached = global.mongoose
+
+if(!cached){
+   cached = global.mongoose = {
+    conn: null, promise: null
+   } 
+}
+
+export async function connectDB(){
+    if(cached.conn){
+        return cached.conn
+    }
+
+    if(!cached.promise){
+        mongoose
+        .connect(MONGODB_URI, {bufferCommands: true, maxPoolSize: 10})
+        .then(() => 
+            mongoose.connection
+        )
+
+        
+    }
+    try{
+        cached.conn = await cached.promise
+    } catch(error){
+        cached.promise = null
+        throw Error
+    }
+
+    return cached.conn
+}
